2ea5086cbff1ac20d779f86bb05040c8c892847e
GunesUstunalp <gunesustunalp@hotmail.com>
2023-02-03 22:58:51 +0300

    Roots are now being generated when RootEnd moves

---
+++ RootEnd.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+var speed = 50
+var rootScene
+
+func _ready():
+	rootScene = preload("res://ARoot.tscn")
+
+func _process(delta):
+	
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 0.2
+		
+	if Input.is_action_pressed("respawn"):
+		position = Vector2(512, 300)
+	
+	velocity = velocity * speed
+	position += velocity * delta
+	
+	create_roots_on_path()
+
+func create_roots_on_path():
+	var rootInstance = rootScene.instance()
+	rootInstance.position = position
+	get_parent().add_child(rootInstance)
--- Sprite.gd
+++ Sprite.gd
@@ -3,23 +3,23 @@ extends Sprite
 var speed = 400
 var angular_speed = PI
 
-func _process(delta):
-	
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-	
-	rotation += angular_speed * direction * delta
-	
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-	elif Input.is_action_pressed("ui_down"):
-		velocity = -1 * Vector2.UP.rotated(rotation) * speed
-	
-	position += velocity * delta
+#func _process(delta):
+#
+#	var direction = 0
+#	if Input.is_action_pressed("ui_left"):
+#		direction = -1
+#	if Input.is_action_pressed("ui_right"):
+#		direction = 1
+#
+#	rotation += angular_speed * direction * delta
+#
+#	var velocity = Vector2.ZERO
+#	if Input.is_action_pressed("ui_up"):
+#		velocity = Vector2.UP.rotated(rotation) * speed
+#	elif Input.is_action_pressed("ui_down"):
+#		velocity =  -1 * Vector2.UP.rotated(rotation) * speed
+#
+#	position += velocity * delta
 
 func _ready():
 	pass
