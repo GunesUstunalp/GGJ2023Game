f37c4113e0dfbf36464019d6bb33fb313183f866
GunesUstunalp <gunesustunalp@hotmail.com>
2023-02-05 01:37:49 +0300

    Rocks now spawn with different scales and rotations

--- RockSpawner.gd
+++ RockSpawner.gd
@@ -1,26 +1,24 @@
 extends Node2D
 var rockImage
 var rockImageScale
-var rockHalfWidth = 40
+var DefaultRockHalfWidth = 40
 var currRockIndex = 0
 
 var animatedRock
 var rockPositionsArr = []
+var rockScalesArr = []
+var rockRotationsArr = []
 var rng = RandomNumberGenerator.new()
 
 signal rock_spawned
 
 func _ready():
 	rockImage = preload('res://Resources/stoneanimation2/0.png')
-	var th = rockHalfWidth * 2
-	var tw = rockHalfWidth * 2
-	var imageSize = rockImage.get_size()
-	rockImageScale = Vector2(tw/imageSize.x, th/imageSize.y)
 	
 	var rootEnd = get_node("RootEnd")
 	rootEnd.connect("found_rock", self, "_on_found_rock")
 	
-	spawnRockAtPosition(Vector2(480,400))
+	spawnRockAtPosition(Vector2(480,400), DefaultRockHalfWidth, 0)
 	
 	animatedRock = preload('res://AnimatedRock.tscn')
 	rng.randomize()
@@ -28,22 +26,27 @@ func _ready():
 func _process(delta):
 	pass
 	
-func spawnRockAtPosition(posToSpawn : Vector2):
+func spawnRockAtPosition(posToSpawn : Vector2, rockHalfWidth, rockRotation):
 	var rockInstance = Sprite.new()
 	rockInstance.texture = rockImage
-	rockInstance.scale = rockImageScale
+	var rockScale = calculateImageScaleBasedOnHalfWidth(rockHalfWidth)
+	rockInstance.scale = rockScale
 	rockInstance.position = posToSpawn
+	rockInstance.rotation_degrees = rockRotation
 	get_node("Rocks").add_child(rockInstance)
 	emit_signal("rock_spawned", posToSpawn, rockHalfWidth, currRockIndex)
 	currRockIndex += 1
 	rockPositionsArr.append(posToSpawn)
+	rockScalesArr.append(rockScale)
+	rockRotationsArr.append(rockRotation)
 
 func _on_found_rock(rootEndPos, foundIndex):
 	print("Found Rock!")
 	
 	var animatedRockInstance = animatedRock.instance()
-	animatedRockInstance.scale = rockImageScale
+	animatedRockInstance.scale = rockScalesArr[foundIndex]
 	animatedRockInstance.position = rockPositionsArr[foundIndex]
+	animatedRockInstance.rotation_degrees = rockRotationsArr[foundIndex]
 	animatedRockInstance.play("found_rock")
 	get_node("AnimatedRocks").add_child(animatedRockInstance)
 	
@@ -58,13 +61,22 @@ func _on_found_rock(rootEndPos, foundIndex):
 
 func spawnRockAtRandomPosition(tryNumber):
 	#print("Spawn Rock at Random Pos")
-	var randX = rng.randf_range(0 + rockHalfWidth, 1024 - rockHalfWidth)
-	var randY = rng.randf_range(300 + rockHalfWidth, 812 - rockHalfWidth)
+	var randHalfWidth = rng.randf_range(DefaultRockHalfWidth/2, DefaultRockHalfWidth)
+	var randX = rng.randf_range(0 + randHalfWidth, 1024 - randHalfWidth)
+	var randY = rng.randf_range(300 + randHalfWidth, 812 - randHalfWidth)
+	var randRotation = rng.randf_range(0,360)
+	
 	if tryNumber > 50:
 		print("Possibly no more space")
 		return
 	
-	if get_node("RootEnd").call("is_there_space_for_a_rock_there", Vector2(randX, randY), rockHalfWidth):
-		spawnRockAtPosition(Vector2(randX, randY))
+	if get_node("RootEnd").call("is_there_space_for_a_rock_there", Vector2(randX, randY), randHalfWidth):
+		spawnRockAtPosition(Vector2(randX, randY), randHalfWidth, randRotation)
 	else:
 		spawnRockAtRandomPosition(tryNumber + 1)
+
+func calculateImageScaleBasedOnHalfWidth(rockHalfWidth):
+	var th = rockHalfWidth * 2
+	var tw = rockHalfWidth * 2
+	var imageSize = rockImage.get_size()
+	return Vector2(tw/imageSize.x, th/imageSize.y)
