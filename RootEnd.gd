7754debca049dedc13ea6ed124baf5cc0e935718
GunesUstunalp <gunesustunalp@hotmail.com>
2023-02-04 23:40:34 +0300

    Re-enabled moving up

--- RootEnd.gd
+++ RootEnd.gd
@@ -41,6 +41,8 @@ func _process(delta):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
 		
 	if Input.is_action_pressed("respawn"):
 		respawn()
